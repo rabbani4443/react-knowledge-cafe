7d48ed0dba4b627d8bb37384c33ea1c6536034c0
rabbani4443 <rabbani07r@gmail.com>
2023-09-12 07:20:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-4xl font-bold text-center pt-10'> react-knowledge-cafe </h1>
+
+    </>
+  )
+}
+
+export default App
